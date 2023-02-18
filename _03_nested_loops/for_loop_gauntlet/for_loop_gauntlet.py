a6b9d72f2e2b1cd2c4264e591840229aeb16c583
jereycui2008 <jeremycuid@gmail.com>
2023-02-17 18:23:14 -0800

    done!

--- _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
+++ _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
@@ -32,7 +32,28 @@ if __name__ == '__main__':
     for i in range(16):
         print("In " + str(i + 2007) + ", I was " + str(i) + " years old")
 
-    for i in range (3):
+    for i in range(3):
         for j in range(3):
             print(str(i) + " " + str(j))
+
+    j = 0
+    for i in range(3):
+        print(str(j + 1) + " " + str(j + 2) + " " + str(j + 3))
+        j += 3
+        i += 1
+
+    for i in range(10):
+        print("")
+        for j in range(1,11):
+            print(str(j + (i*10)), end=" ")
+
+    for i in range(7):
+        print("")
+        for j in range(i):
+            print("*", end="")
+
+    print("")
+
+    for i in range(101):
+        print(str(100 - i))
 
